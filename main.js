04e51765e4b10465013dfb173353a89ef1e4a30e
AlexDroid <101382932+alexdroidcuba@users.noreply.github.com>
2023-05-07 22:59:15 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//import shoelace from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.2.0/+esm'
+//import shoelaceStyleshoelace from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.2.0/+esm'
+//import vaadinvaadinCore from 'https://cdn.jsdelivr.net/npm/@vaadin/vaadin-core@23.3.6/+esm'
+
+/*import { h, Component, render } from 'https://esm.sh/preact';
+import htm from 'https://esm.sh/htm';
+const html = htm.bind(h);
+
+const app = html`<h1>Hello World!</h1>`;
+render(app, document.body);*/
