8b17a5f82b38fe1fff33b3026e157cdc85c0f4bc
AlexDroid <101382932+alexdroidcuba@users.noreply.github.com>
2023-05-07 23:22:01 -0400

    uncomment

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 //import shoelaceStyleshoelace from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.2.0/+esm'
 //import vaadinvaadinCore from 'https://cdn.jsdelivr.net/npm/@vaadin/vaadin-core@23.3.6/+esm'
 
-/*import { h, Component, render } from 'https://esm.sh/preact';
+import { h, Component, render } from 'https://esm.sh/preact';
 import htm from 'https://esm.sh/htm';
 const html = htm.bind(h);
 
 const app = html`<h1>Hello World!</h1>`;
-render(app, document.body);*/
+render(app, document.body);
